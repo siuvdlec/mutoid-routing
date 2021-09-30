144765702ab3f6b541cbd29f8e3f99f2f122f756
Iacopo <siuvdlec@gmail.com>
2021-09-30 22:59:27 +0200

    try path-to-regexp

--- example/index.ts
+++ example/index.ts
@@ -1,66 +1,73 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { pipe } from 'fp-ts/function'
-import { end, format, imap, int, lit, parse, rgx, Route, str, zero } from '../src/routing'
+import { compile } from 'path-to-regexp'
 
-interface Home {
-    readonly _tag: 'Home'
-}
+// interface Home {
+//     readonly _tag: 'Home'
+// }
 
-interface NotFound {
-    readonly _tag: 'NotFound'
-}
+// interface NotFound {
+//     readonly _tag: 'NotFound'
+// }
 
-const home: Location = { _tag: 'Home' }
+// const home: Location = { _tag: 'Home' }
 
-const notFound: Location = { _tag: 'NotFound' }
+// const notFound: Location = { _tag: 'NotFound' }
 
-type Location = Home | NotFound
+// type Location = Home | NotFound
 
-// https://www.facile.it/auto/dettaglio/lancia-nuova_ypsilon-10_hybrid_silver_ss_70cv-850CJ58.html
+// const keys: any[] = []
+// const regexp = pathToRegexp('/foo/:bar(\\d+)-:xxx', keys)
 
-const defaults = end
-const homeMatch = lit('home').then(str('id')).then(end)
+// const toPath = compile('/user/:id', { encode: encodeURIComponent })
 
-const aaa = lit('auto')
-    .then(lit('dettaglio'))
-    .then(str('dettaglio'))
-    .then(
-        rgx(
-            'hello',
-            new RegExp(/(.*)-([\w]+)\.html/),
-            _ppp => 'xx' as const,
-            _ppp => 'zz'
+export type ExtractRouteOptionalParam<T extends string, U = string | number | boolean> = T extends `${infer Param}?`
+    ? { [k in Param]?: U }
+    : T extends `${infer Param}*`
+    ? { [k in Param]?: U }
+    : T extends `${infer Param}+`
+    ? { [k in Param]: U }
+    : { [k in T]: U }
+
+export type ExtractRouteParams<T extends string, U = string | number | boolean> = string extends T
+    ? { [k in string]?: U }
+    : T extends `${infer _Start}:${infer ParamWithOptionalRegExp}/${infer Rest}`
+    ? ParamWithOptionalRegExp extends `${infer Param}(${infer _RegExp})`
+        ? ExtractRouteOptionalParam<Param, U> & ExtractRouteParams<Rest, U>
+        : ExtractRouteOptionalParam<ParamWithOptionalRegExp, U> & ExtractRouteParams<Rest, U>
+    : T extends `${infer _Start}:${infer ParamWithOptionalRegExp}`
+    ? ParamWithOptionalRegExp extends `${infer Param}(${infer _RegExp})`
+        ? ExtractRouteOptionalParam<Param, U>
+        : ExtractRouteOptionalParam<ParamWithOptionalRegExp, U>
+    : // eslint-disable-next-line @typescript-eslint/ban-types
+      {}
+
+const myCompile = <
+    Path extends string = string,
+    Params extends { [K: string]: { toString: () => string } | undefined } = ExtractRouteParams<
+        Path,
+        { toString: () => string }
+    >
+>(
+    s: Path
+): ((p: Params) => string) => {
+    return (p: Params) =>
+        pipe(
+            Object.keys(p).reduce((c, k) => ({ ...c, [k]: p[k]?.toString() }), {}),
+            compile(s)
         )
-    )
-    .then(end)
+}
 
-const y = pipe(
-    str('id'),
-    imap(
-        ({ id }) => ({ userId: id }),
-        ({ userId }) => ({ id: userId })
-    )
-)
+const ddd = myCompile('/user/:id/:hhh')
+const ddd1 = myCompile('/user/home.html')
 
-const z = pipe(str('id'))
+class Hei {
+    toString() {
+        return '1'
+    }
+}
 
-const router = zero<Location>()
-    .alt(defaults.parser.map(() => home))
-    .alt(homeMatch.parser.map(c => home))
-    .alt(aaa.parser.map(c => home))
-    .alt(y.parser.map(c => home))
-    .alt(z.parser.map(c => home))
+const dd = ddd({ id: 1, hhh: new Hei() })
+const aa = ddd1({})
 
-const parseLocation = (s: string): Location => parse(router, Route.parse(s), notFound)
-
-const dd1 = format(y.formatter, { userId: 'asdf' })
-const dd = format(z.formatter, { id: 'sfg' })
-
-// Route
-
-// Match = Parser + Formatter
-
-// ---
-
-// add RouteMatcher ?
-
-// add Router in array di RouteMatcher ?
+console.log(dd, aa)
